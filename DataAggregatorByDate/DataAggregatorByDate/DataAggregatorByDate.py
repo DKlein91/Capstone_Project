f06d3e21cc1662068b30ca67819cd88bf872f809
D. Klein <DKlein91@users.noreply.github.com>
2018-04-05 07:31:26 -0400

    Initial Push, only initial variables defined

---
+++ DataAggregatorByDate/DataAggregatorByDate/DataAggregatorByDate.py
@@ -0,0 +1,14 @@
+import datetime
+
+startYear = 1601
+startMonth = 1
+startDay = 1
+endYear = 3000
+endMonth = 12
+endDay = 31
+createdDates = False
+accessedDates = False
+modifiedDates = False
+deletedDates = False
+
+
